53df434768c7f840bb7bf9bdb31a972af2249e05
Onserioo <joshuaomirera@gmail.com>
2023-02-10 08:42:37 +0300

    .

---
+++ src/Sample/Example1.java
@@ -0,0 +1,10 @@
+package Sample;
+
+public class Example1 {
+        public static void main(String[] args) {
+
+            System.out.println("Hello java..");
+
+        }
+}
+
---
+++ src/Sample/Example3.java
@@ -0,0 +1,19 @@
+package Sample;
+
+public class Example3 {
+
+    public static void main(String[] args) {
+
+        int var1;
+        int var2;
+        var1 = 1024;
+        var2 = var1 / 2;
+        System.out.println("var1 = " + var1);
+        System.out.println("var2 = " + var2);
+        System.out.println("var1 + var2 = " + (var1 + var2));
+        System.out.println("var1 - var2 = " + (var1 - var2));
+        System.out.println("var1 * var2 = " + (var1 * var2));
+        System.out.println("var1 / var2 = " + (var1 / var2));
+
+    }
+}
