4b8a02f4f2d822dab39aa763b4d7b5abfc7e7f7e
Onserioo <joshuaomirera@gmail.com>
2023-02-16 17:37:21 +0300

    .

---
+++ src/Sample/ScannerInput.java
@@ -0,0 +1,16 @@
+package Sample;
+
+import java.util.Scanner;
+public class ScannerInput {
+    public static void main(String[] args) {
+        //... Initialize Scanner to read from console.
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number : ");
+        int a = input.nextInt();
+        System.out.print("Enter second number: ");
+        int b = input.nextInt();
+        System.out.print("Enter last number : ");
+        int c = input.nextInt();
+        System.out.println("Average is " + (a+b+c)/3);
+    }
+}
